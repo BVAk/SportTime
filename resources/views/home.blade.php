24838e30ef50ae76a2faa95ddfa1f3e252aacfc2
BVAk <bekkviktoria55@gmail.com>
2019-09-16 12:53:44 +0300

    admin dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,12 @@
-@extends('layouts.app')
+@extends('layouts.apps')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+<div class="ftco-blocks-cover-1">
+<div class="site-section-cover overlay" data-stellar-background-ratio="0.5" style="background-image: url('images/hero_1.jpg')">
+</div> 
+</div>
+<div class="site-section">
+      <div class="container">  
+            <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
@@ -21,4 +24,4 @@
         </div>
     </div>
 </div>
-@endsection
+
