8d7a598e42ff8d3ce95a81a623d7d09298b5c6cb
BVAk <bekkviktoria55@gmail.com>
2019-10-20 21:18:24 +0300

    landing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -133,21 +133,6 @@
 
 
 
-<footer class="site-footer">
-      <div class="container">
-        <div class="row pt-5 mt-5 text-center">
-          <div class="col-md-12">
-            <div class="border-top pt-5">
-              <p>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            &copy; Copyright Fitness Time <script>document.write(new Date().getFullYear());</script>. Developed and designed by Bekk
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
 
     </div>
 
