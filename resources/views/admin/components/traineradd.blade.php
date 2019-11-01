1738a9b849e815657e154c60625cb7fbbe2c7829
BVAk <bekkviktoria55@gmail.com>
2019-11-01 13:15:40 +0200

    change trainer add

--- resources/views/admin/components/traineradd.blade.php
+++ resources/views/admin/components/traineradd.blade.php
@@ -2,64 +2,82 @@
 
 @section('content')
 <div class="container-fluid">
-    <form role="form" class="col-md-12 go-right" action="{{ url('/admin/inserttrainer') }}" method="post"
-          enctype="multipart/form-data">
+    <form role="form" class="col-md-12 go-right" action="{{ url('/admin/inserttrainer') }}" method="post" enctype="multipart/form-data">
         {{ csrf_field() }}
-<label>Створення нового тренера</label>
-        <div class="form-group">
-            <label for="name">ФІО</label>
-            <input id="name" name="name" type="text" class="form-control" value="" required>
-
+        <div class="row">
+            <label>Створення нового тренера</label>
         </div>
+        <div class="row">
+        <div class="col">
 
-
-        <div class="form-group">
-            <label for="birth">Дата народження</label>
-            <input id="birth" name="birth" type="date" class="form-control" value="" required>
-
-        </div>
-
-        <div class="form-group">
-            <label for="start">Початок роботи</label>
-            <input id="start" name="start" type="date" class="form-control" value=""
-                   required>
-
-        </div>
-
-        <div class="form-group">
-            <label for="phone">Мобільний номер телефону</label>
-            <input id="phone" name="phone" type="text" class="form-control" value="" required>
-
-        </div>
-        <label for="phone">Тренування</label>
-
-        <div class="panel panel-default">
-
-            <div class="panel-body">
-                <div class="col-md-6">
-                    <select id="select" name="trainings[]" class="selectpicker" style="font-size: 20px;"
-                            required>
-                        <option value=""> ---</option>
-                        @foreach ($training as $training)
-                            <option value="{{$training->id}}"> {{$training->name}}</option>
-                        @endforeach
-                    </select>
-
-                </div>
+            <div class="form-group">
+                <label for="name">ФІО</label>
+                <input id="name" name="name" type="text" class="form-control" value="" required>
 
             </div>
         </div>
+        <div class="col">
 
+            <div class="form-group">
+                <label for="birth">Дата народження</label>
+                <input id="birth" name="birth" type="date" class="form-control" value="" required>
 
-        <div class="form-group">
-            <label for="photo">Фото тренера</label>
-            <input type="file" name="photo"><br>
+            </div>
         </div>
-
-        <div class="form-group">
-            <input class="btn btn-success center-block btn-lg" type="submit" value="Додати">
         </div>
+        <div class="row">
+        <div class="col">
 
+            <div class="form-group">
+                <label for="start">Початок роботи</label>
+                <input id="start" name="start" type="date" class="form-control" value="" required>
+
+            </div>
+        </div>
+        <div class="col">
+
+            <div class="form-group">
+                <label for="phone">Мобільний номер телефону</label>
+                <input id="phone" name="phone" type="text" class="form-control" value="" required>
+
+            </div>
+        </div>
+        </div>
+        <div class="row">
+        <div class="col">
+            <div class="form-group">
+                <label for="phone">Тренування</label>
+
+                <div class="panel panel-default">
+
+                    <div class="panel-body">
+                        <div class="col-md-6">
+                            <select id="select" name="trainings[]" class="selectpicker" style="font-size: 20px;" required>
+                                <option value=""> ---</option>
+                                @foreach ($training as $training)
+                                <option value="{{$training->id}}"> {{$training->name}}</option>
+                                @endforeach
+                            </select>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="form-group">
+                <label for="photo">Фото тренера</label>
+                <input type="file" name="photo"><br>
+            </div>
+
+        </div>
+        </div>
+        <div class="row">
+        
+            <div class="form-group">
+                <input class="btn btn-success center-block btn-lg" type="submit" value="Додати">
+            </div>
+        </div>
 
     </form>
 
