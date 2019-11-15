acfbc48ba36326dc5ff266aa6393b711977c8c6a
BVAk <bekkviktoria55@gmail.com>
2019-11-15 13:26:14 +0200

    change abonnement add

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -170,7 +170,7 @@ class AdminController extends Controller
     public function profileClients(User $id)
     {
         $users = User::where('id', '=', $id->id)->get();
-        $userabonnement = DB::table('usersabonnements')->join('abonnements','abonnements.id','=','usersabonnements.abonnement_id')->where('usersabonnements.user_id', '=', $id->id)->where('usersabonnements.end','>=', new \DateTime('now'))->orwhere('usersabonnements.amount','>',0)->get();
+        $userabonnement = DB::table('usersabonnements')->where('usersabonnements.user_id', '=', $id->id)->join('abonnements','abonnements.id','=','usersabonnements.abonnement_id')->where('usersabonnements.end','>=', new \DateTime('now'))->orwhere('usersabonnements.amount','>',0)->get();
         $privateschedule = DB::table('privateschedule')->join('trainings', 'privateschedule.training_id', '=', 'trainings.id')->join('trainers', 'privateschedule.trainer_id', '=', 'trainers.id')->where('user_id', '=', $id->id)->where('date', '>=', new \DateTime('now'))->select('trainings.name as training_name', 'trainers.name as trainer_name', 'privateschedule.date as privateschedule_date', 'privateschedule.endtrain as privateschedule_endtrain', 'privateschedule.id as privateschedule_id')->get();
         $trainergym2 = DB::table('traintrain')->join('trainers', 'traintrain.trainer_id', '=', 'trainers.id')->join('trainings', 'traintrain.training_id', '=', 'trainings.id')->where('training_id', '=', '1')->orwhere('training_id', '=', '2')->select('trainers.id as id', 'trainers.name as trainer_name', 'start', 'image', 'trainings.name as training_name')->get();
         $abonnement = DB::table('abonnements')->get();
